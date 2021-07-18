a5c6aee9962bf9401aae983e57e38292b10fae6d
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-18 17:36:56 -0500

    Finishing touches

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ const tweets = require("./routes/api/tweets");
 const User = require("./models/User")
 
 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
